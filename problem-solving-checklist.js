9f84960feb288c0d11c088d559d3e3bd6e5cf702
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-02-15 17:46:58 +0600

    update

---
+++ problem-solving-checklist.js
@@ -0,0 +1,21 @@
+//1.conversation
+function feetToInch(){}
+
+
+//2.conversion
+
+function centimeeterToMeter(){}
+
+
+//3.calculation
+
+function pageRequirments(){}
+
+
+//4.friend name lenth in an array
+
+function bestFriend(){}
+
+//5.will stop the loop if the array has any negative number and returns all the posetive number before the negative number
+
+function onlyPosetive(){}
