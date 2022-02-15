4c9829b14bd11824371d226d3f46fec632928b05
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-02-15 18:02:32 +0600

    update

--- problem-solving-checklist.js
+++ problem-solving-checklist.js
@@ -1,11 +1,21 @@
 //1.conversation
-function feetToInch(){}
+function feetToInch(feet){
+    const inch=12*feet;
+    return inch;
+}
 
+const Totalinch=feetToInch(10);
+console.log(Totalinch);
 
 //2.conversion
 
-function centimeeterToMeter(){}
+function centimeeterToMeter(centimeeter){
+    const meeter=centimeeter*0.01;
+    return meeter;
+}
 
+const meeter=centimeeterToMeter(1000);
+console.log(meeter);
 
 //3.calculation
 
