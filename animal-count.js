d4cab22f41d321927e8ac3732382cf23573b0e6a
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-02-15 12:14:45 +0600

    update

---
+++ animal-count.js
@@ -0,0 +1,27 @@
+
+function animalCount(miles){
+    const first10MilesAnimalDensity=10;
+    const second10MilesAnimalDensity=20;
+    const third10MilesAnimalDensity=30;
+    const first10Miles=10*first10MilesAnimalDensity;
+    
+
+        if(miles<=10){
+            const firstTotalAnimals=first10Miles;
+            return firstTotalAnimals;
+        }
+        else if(miles<=20){
+            const second10Miles=(miles-10)*second10MilesAnimalDensity;
+            const secondTotalAnimals=first10Miles+second10Miles;
+            return secondTotalAnimals;
+        }
+        else{
+            const second10Miles=10*second10MilesAnimalDensity;
+            const third10Miles=(miles-20)*third10MilesAnimalDensity;
+            const thirdTotalAnimals=first10Miles+second10Miles+third10Miles;
+            return thirdTotalAnimals;
+        }
+}
+
+const miles=animalCount(35);
+console.log('Total Animals : ', miles);
