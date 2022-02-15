ea1c7e161d651eb1ec0f16114897692c3862ae9a
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-02-15 18:22:54 +0600

    update

--- problem-solving-checklist.js
+++ problem-solving-checklist.js
@@ -7,8 +7,8 @@ function feetToInch(feet){
 const Totalinch=feetToInch(10);
 console.log(Totalinch);
 
-//2.conversion
 
+//2.conversion
 function centimeeterToMeter(centimeeter){
     const meeter=centimeeter*0.01;
     return meeter;
@@ -17,13 +17,21 @@ function centimeeterToMeter(centimeeter){
 const meeter=centimeeterToMeter(1000);
 console.log(meeter);
 
-//3.calculation
 
-function pageRequirments(){}
+//3.calculation
+function pageRequirments(book1,book2,book3){
+    let book1Page=100*book1;
+    let book2Page=100*book2;
+    let book3Page=100*book3;
+    let totalPages=book1Page+book2Page+book3Page;
+    return totalPages;
+
+}
+const totalPages=pageRequirments(2,3,3);
+console.log('Total Pages : ',totalPages); 
 
 
 //4.friend name lenth in an array
-
 function bestFriend(){}
 
 //5.will stop the loop if the array has any negative number and returns all the posetive number before the negative number
